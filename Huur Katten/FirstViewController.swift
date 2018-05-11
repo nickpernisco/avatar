a7ca069c1f26e40f6191d3e5beb899a72319b264
nickpernisco <nick@pernisco.com>
2018-05-11 18:31:01 +0200

    Initial Commit

---
+++ Huur Katten/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Huur Katten
+//
+//  Created by Nick Pernisco on 5/11/18.
+//  Copyright © 2018 Nick Pernisco. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
