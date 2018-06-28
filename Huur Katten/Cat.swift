8382bd8910d6946e79bccaa63064ae936db8888d
nickpernisco <nick@pernisco.com>
2018-06-28 17:57:05 +0200

    First commit

---
+++ Huur Katten/Cat.swift
@@ -0,0 +1,26 @@
+//
+//  Cat.swift
+//  Huur Katten
+//
+//  Created by Nick Pernisco on 5/27/18.
+//  Copyright © 2018 Nick Pernisco. All rights reserved.
+//
+
+import UIKit
+
+class Cat {
+    var photoImage: UIImage
+    var ratingImage: UIImage
+    var name: String
+    var neighborhood: String
+    var confirmedKills: Int
+    
+    init(photoImage: UIImage, ratingImage: UIImage, name: String, neighborhood: String, confirmedKills: Int) {
+        self.photoImage = photoImage
+        self.ratingImage = ratingImage
+        self.name = name
+        self.neighborhood = neighborhood
+        self.confirmedKills = confirmedKills
+    }
+    
+}
